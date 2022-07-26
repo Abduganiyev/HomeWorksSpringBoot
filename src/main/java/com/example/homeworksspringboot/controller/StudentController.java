b9af76ddde09ca031eb9f442e8a1eea50062188c
Abduganiyev <mr.show050@gmail.com>
2022-07-26 07:40:46 +0500

    initial commit

--- src/main/java/com/example/homeworksspringboot/controller/StudentController.java
+++ src/main/java/com/example/homeworksspringboot/controller/StudentController.java
@@ -18,7 +18,7 @@ public class StudentController {
     }
 
     @PostMapping
-    private Student save(@RequestBody Student student) {
+    private Student saveStudent(@RequestBody Student student) {
         return studentRepository.save(student);
     }
 
